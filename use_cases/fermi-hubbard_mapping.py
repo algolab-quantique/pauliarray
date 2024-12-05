eb5d74dccf78631f760b3bf4b082db1d2ae35efc
oneminimax <maxime.dion@me.com>
2024-12-05 14:41:33 -0500

    test mapping

--- use_cases/fermi-hubbard_mapping.py
+++ use_cases/fermi-hubbard_mapping.py
@@ -9,24 +9,25 @@ from pauliarray.mapping.fermion import BravyiKitaev, FermionMapping, JordanWigne
 
 # %%
 
-lattice_dim = (2, 2)
+lattice_dim = (2, 4)
 num_sites = np.prod(lattice_dim)
 num_qubits = 2 * num_sites
 
 map_matrix = np.tril(np.random.randint(0, 2, size=(num_qubits, num_qubits)), k=-1) + np.eye(num_qubits, dtype=int)
 # print(map_matrix)
 
-mapper = FermionMapping(map_matrix, "random")
+
+mapper = JordanWigner(num_qubits)
 # mapper = Parity(num_qubits)
-# mapper = JordanWigner(num_qubits)
 # mapper = BravyiKitaev(num_qubits)
+# mapper = FermionMapping(map_matrix, "random")
 
 fermi_hubbard_model = FermiHubbardModel(SquareLattice(*lattice_dim), 1)
 
 qiskit_fermionic_op = fermi_hubbard_model.second_q_op()
 
 hamiltonian = mapper.assemble_qubit_hamiltonian_from_sparses(*extract_fermionic_op(qiskit_fermionic_op))
-# print(hamiltonian.inspect())
+print(hamiltonian.inspect())
 
 pauli_weights, counts = np.unique(hamiltonian.paulis.num_non_ids, return_counts=True)
 order = np.argsort(pauli_weights)
